63bf6376a41a124ed9dbb5aa3b39c1280c644d2b
TimeFixty <116175496+TimeFixty@users.noreply.github.com>
2022-11-11 10:57:25 +0100

    Add files via upload

---
+++ FizlyHub.lua
@@ -0,0 +1,423 @@
+local TeleportService = game:GetService("TeleportService")
+
+
+
+
+
+---@diagnostic disable-next-line: undefined-global
+--Functions
+
+
+-----------
+
+
+
+
+
+
+
+
+
+
+
+function LoadFizly_Hub()
+
+	local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+	
+	
+	local Window = Rayfield:CreateWindow({
+		Name = "Fizly Hub",
+		LoadingTitle = "Welcome To Fizly Hub v.1.0",
+		LoadingSubtitle = "by Time Fixty#1570",
+		ConfigurationSaving = {
+			Enabled = true,
+			FolderName = "Fizly Hub",
+			FileName = "Fizly Hub File"
+		},
+		Discord = {
+			Enabled = false,
+			Invite = "", 
+			RememberJoins = true
+	
+		},
+		KeySystem = true, 
+		KeySettings = {
+			Title = "Fizly Hub Checking",
+			Subtitle = "Key System",
+			Note = "Join the discord for Key (soon) ",
+			SaveKey = false,
+			GrabKeyFromSite = false,
+			Key = "ImUsingIt"
+	
+	
+		
+		}
+	})
+	--Functions
+
+	--GamesCorrect
+	
+		
+	
+	 --games =   {
+      	
+		
+	
+	--  }
+    
+	
+
+
+	
+	 
+
+	--Patched
+		function Patched()
+
+		end
+		--OutDated
+		function OutDated()
+			
+		end
+         --Error
+		 function Errors()
+			
+		 end
+
+
+
+	
+	---@diagnostic disable-next-line: undefined-global
+		function Notes()
+			local Tab = Window:CreateTab("Note", 4483362458) 
+			local Section = Tab:CreateSection("Notes Info")
+			local Paragraph = Tab:CreateParagraph({Title = "V.1.0 Note (still Support)", Content = "Version 1.0 Note"})
+
+
+
+                --Added
+				local Section = Tab:CreateSection("Added")
+				local Label1 = Tab:CreateLabel("Added Note")
+
+				--Fixed
+				local Section = Tab:CreateSection("Fixed")
+				local Label2 = Tab:CreateLabel("Fixed Note")
+				
+                    
+				--Deleted
+				local Section = Tab:CreateSection("Deleted")
+				local Label3 = Tab:CreateLabel("Deleted Note")
+			
+
+			
+		end
+		
+
+
+
+	 function LoadAutoStrat_1()
+
+		local Tab1 = Window:CreateTab("Load Auto Strats", 4483362458) -- Title, Image
+		local Section1 = Tab1:CreateSection("Auto Strats")
+
+		
+         --END v1.0
+		 
+			 
+		
+		local Paragraph = Tab1:CreateParagraph({Title = "Auto Strat V1.0 ", Content = "Version 1.0  (old) (V2.1 Will be good Auto Strat)"})
+
+	
+		local Paragraph = Tab1:CreateParagraph({Title = "Towers need", Content = "Dj booth,Commander,Accel,Farm,Cowboy."})
+		local Paragraph = Tab1:CreateParagraph({Title = "Golden Perks", Content = "Cowboy."})
+		local Paragraph = Tab1:CreateParagraph({Title = "Time", Content = "13min 38s"})
+		local Paragraph = Tab1:CreateParagraph({Title = "Win Rate", Content = "97%"})
+		local Paragraph = Tab1:CreateParagraph({Title = "Map", Content = "ToyBoard"})
+		local Paragraph = Tab1:CreateParagraph({Title = "Mode", Content = "Molten"})
+		local Paragraph = Tab1:CreateParagraph({Title = "Coins Per/H", Content = "2000/2500"})
+--Buttons v.1.0
+
+local Button = Tab1:CreateButton({
+	Name = "Auto Strat v1.0",
+	Callback = function()
+		Rayfield:Notify({
+			Title = "Are you sure",
+			Content = "Are you sure to load the Auto Strat?",
+			Duration = 6.5,
+			Image = 4483362458,
+			Actions = { 
+				Ignore = {
+					Name = "Load Auto Strat v.1.0",
+					Callback = function()
+						
+             --Load Auto Strat
+			 
+					end
+				},
+			},
+		})
+
+	end,
+})
+
+	     --END V.2.0
+		 
+		 ----------------
+	end
+	
+		--Destroy Gui
+	function Destroy_Gui()
+		local Tab2 = Window:CreateTab("Destroy Gui", 4483362458) -- Title, Image
+		local Section2 = Tab2:CreateSection("Destroy Gui")
+
+		local Button = Tab2:CreateButton({
+			Name = "Destroy Gui",
+			Callback = function()
+
+				Rayfield:Notify({
+					Title = "Are you Sure",
+					Content = "Are you Sure to Destroy Gui?",
+					Duration = 6.5,
+					Image = 4483362458,
+					Actions = { -- Notification Buttons
+						Ignore = {
+							Name = "Im Sure",
+							Callback = function()
+								Rayfield:Destroy()
+							end
+						},
+					},
+				})
+			end,
+		})
+	end
+	
+	--Info
+	
+	function Info()
+		local Tab3 = Window:CreateTab("Info", 4483362458) -- Title, Image
+		local Section3 = Tab3:CreateSection(" Info")
+		
+		local Paragraph = Tab3:CreateParagraph({Title = "soon", Content = "Soon!"})
+	end
+	
+	--Credits
+	
+	function Credits_()
+		local Tab4 = Window:CreateTab("Credits", 4483362458) -- Title, Image
+		local Section4 = Tab4:CreateSection("Credits")
+		local Paragraph = Tab4:CreateParagraph({Title = "soon", Content = "Soon!"})
+		
+	end
+
+
+
+
+
+
+
+
+
+
+-----------Load Function
+LoadAutoStrat_1()
+Destroy_Gui()
+Info()
+Credits_()
+Notes()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--------------------
+
+
+
+
+
+end
+	------
+	--End 
+	
+	----------------------
+
+
+
+
+
+
+
+
+	-----------------
+	
+	--Money Maker Hub (Fizly)
+	Money_Maker_Hub_Fizly = function()
+		local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+		local Window = Rayfield:CreateWindow({
+			Name = "Soon",
+			LoadingTitle = "Coming Soon",
+			LoadingSubtitle = "by Time Fixty",
+			ConfigurationSaving = {
+				Enabled = true,
+				FolderName = nil, -- Create a custom folder for your hub/game
+				FileName = "Big Hub"
+			}
+		})
+		Rayfield:Notify({
+			Title = "Notification Title",
+			Content = "Notification Content",
+			Duration = 6.5,
+			Image = 4483362458,
+			Actions = { -- Notification Buttons
+				Ignore = {
+					Name = "Okay i wait then",
+					Callback = function()
+						print(":) Maybe In Christmas")
+					end
+				},
+			},
+		})
+		wait(7)
+		Choose_Load()
+	end
+
+
+
+	----------
+	IncorrectGame = function()
+		local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+			local Window1 = Rayfield:CreateWindow({
+	Name = "Checking System",
+	LoadingTitle = "Checking",
+	LoadingSubtitle = "Loading",
+	ConfigurationSaving = {
+	Enabled = true,
+	FolderName = "Checking System InCorrect",
+	FileName = "Checking System InCorrect"
+	}
+	})
+		Rayfield:Notify({
+			Title = "InCorrect",
+			Content = "InCorrect Game",
+			Duration = 5.9,
+			Image = 4483362458,
+		})
+	  end
+	GameCheck = function()
+	local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+	repeat wait()  until game:IsLoaded()
+	if game.PlaceId == 3260590327 then
+		
+	local Window2 = Rayfield:CreateWindow({
+	Name = "Checking System",
+	LoadingTitle = "Checking",
+	LoadingSubtitle = "Loading",
+	ConfigurationSaving = {
+	Enabled = true,
+	FolderName = "Check System Correct",
+	FileName = "Check System Correct"
+	}
+	})
+	Rayfield:Notify({
+	Title = "Correct",
+	Content = "Correct Game",
+	Duration = 6.5,
+	Image = 4483362458,
+	Actions = { 
+	Ignore = {
+		Name = "Click to Load",
+		Callback = function ()
+			Rayfield:Destroy()
+			wait(1)
+			local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+        local Window  = Rayfield:CreateWindow({
+Name = "choose Hub",
+LoadingTitle = "By Time Fixty#1570",
+LoadingSubtitle = "Loading",
+ConfigurationSaving = {
+Enabled = false,
+FolderName = "Choose Hubs",
+FileName = "Choose Hubs"
+},
+Discord = {
+	Enabled = true,
+	Invite = "3thBJx2axb", 
+	RememberJoins = false
+}
+})
+
+local Choose_Load = Window:CreateTab("Choose Hub ", 4483362458) 
+local Choose_Load_Section = Choose_Load:CreateSection("Choose hub (Hubs was Made By Time Fixty ) Check Info")
+local Button1 = Choose_Load:CreateButton({
+	Name = "Load Fizly Hub (My Own Hub with My Strats Get more info in Credits or Info)",
+	Callback = function()
+		Rayfield:Notify({
+            Title = "Are You sure",
+            Content = "Are You Sure to Open Fizly Hub?",
+            Duration = 6.5,
+            Image = 4483362458,
+            Actions = { -- Notification Buttons
+                Ignore = {
+                    Name = "Im Sure!",
+                    Callback = function()
+						LoadFizly_Hub()
+                       
+                    end
+                },
+            },
+        })
+	end,
+})
+
+
+local Button2 = Choose_Load:CreateButton({
+	Name = "Load Money Maker Hub (they created script but i create Hub)",
+	Callback = function()
+        Rayfield:Notify({
+            Title = "Are You sure",
+            Content = "Are You Sure to Open Money Maker Hub?",
+            Duration = 6.5,
+            Image = 4483362458,
+            Actions = { -- Notification Buttons
+                Ignore = {
+                    Name = "Im Sure!",
+                    Callback = function()
+                        Money_Maker_Hub_Fizly()
+                    end
+                },
+            },
+        })
+	end,
+})
+
+
+		end
+	},
+	},
+	})
+	  else
+	IncorrectGame()
+	wait(6)
+			local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+	Rayfield:Destroy()
+	  end
+		end
+		
+	
+	
+	
+	
+	GameCheck()
